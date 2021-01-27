d8938e1417ebd9a834fd4a0c81bbcf9d43decea6
Mandy Wade <lightninggirl413@hotmail.com>
2021-01-27 17:36:29 -0500

    Added Mumblings

--- Controllers/MumblingController.cs
+++ Controllers/MumblingController.cs
@@ -11,5 +11,25 @@ namespace ExplosionAPI.Controllers
     [ApiController]
     public class MumblingController : ControllerBase
     {
+        [HttpGet("{characterString}")]
+        public string Mumbling(string characterString)
+        {
+            var answer = "";
+            for (var index = 0; index < characterString.Length; index++)
+            {
+                var upperCase = characterString[index].ToString().ToUpper();
+                var lowerCase = characterString[index].ToString().ToLower();
+                answer += upperCase;
+                for (var i = 0; i < index; i++)
+                {
+                    answer += lowerCase;
+                }
+                if (index != characterString.Length - 1)
+                {
+                    answer += "-";
+                }
+            }
+            return answer;
+        }
     }
 }
